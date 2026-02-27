36cae1c677485c3324ac3faf800b432612ca235f
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-02-27 16:01:42 +0800

    Add plugin file

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Clearance Section for WooCommerce
+ * Description: Move old stock easily by adding a dedicated clearance section to WooCommerce.
+ * Version: 1.0.0
+ * Author: Adrian Duffell
+ * Author URI: https://adrianduffell.com
+ * Text Domain: wc-clearance
+ * License: GPL-3.0+
+ */
