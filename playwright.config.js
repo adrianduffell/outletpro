2f4ae3e2e793068f271741e79c96a43a2923e59f
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-02-28 18:32:11 +0800

    Update playwright config with actual wpenv test port

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require( '@playwright/test' );
 
 module.exports = defineConfig( {
 	use: {
-		// Port 1111 matches the wp-env "port" setting in .wp-env.json.
-		baseURL: process.env.WP_BASE_URL || 'http://localhost:1111',
+		// Port 1112 matches the wp-env "port" setting in .wp-env.json.
+		baseURL: process.env.WP_BASE_URL || 'http://localhost:1112',
 	},
 	testDir: './tests/e2e',
 } );
