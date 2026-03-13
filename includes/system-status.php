a68b83ad40d90e6bdeeec177451768f52d6085a5
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-03-13 09:37:45 +0800

    Fix lint issue

--- includes/system-status.php
+++ includes/system-status.php
@@ -38,7 +38,7 @@ function add_system_status_section_hook(): void {
 			esc_html( (string) $label ),
 			// Special handling for the canonical term ID item to highlight the error state.
 			// todo: consider generalising this for other items.
-			( $value == 'Not found' && str_contains( $label, 'Canonical term ID' ) ? '<mark class="error"><span>Canonical term not found.</span></mark>' : esc_html( (string) $value ) ),
+			( 'Not found' === $value && str_contains( $label, 'Canonical term ID' ) ? '<mark class="error"><span>Canonical term not found.</span></mark>' : esc_html( (string) $value ) ),
 			esc_attr( $id )
 		);
 	}
