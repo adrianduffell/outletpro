f9d4bf521bb47dd4177b34dc9887b4bfb52072c6
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-04 12:37:07 +0800

    Add a deliberate lint issue (retry)

--- wc-clearance.php
+++ wc-clearance.php
@@ -171,8 +171,8 @@ function enqueue_build_assets_hook(): void {
 }
 add_action( 'enqueue_block_editor_assets', 'WC_Clearance\enqueue_build_assets_hook' );
 
-// Hook into WordPress.
+// Hook into WordPress
 add_action( 'init', 'WC_Clearance\init_hook', 20 );
 add_action( 'admin_init', 'WC_Clearance\admin_init_hook' );
 
-			register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
+register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
