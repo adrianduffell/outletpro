3bde23c41cebe302ce8ec7bd2a2634e0fb7d8663
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-09 14:16:38 +0800

    Update icons to 2-chevron design (#321)

--- src/blocks/clearance-badge/icon.tsx
+++ src/blocks/clearance-badge/icon.tsx
@@ -9,13 +9,12 @@ const ClearanceBadgeIcon = (
 			fill="currentColor"
 		/>
 
-		<path d="M7 7.7H8.5L11.5 11.7L8.5 15.7H7L10 11.7L7 7.7Z" fill="#fff" />
 		<path
-			d="M10 7.7H11.5L14.5 11.7L11.5 15.7H10L13 11.7L10 7.7Z"
+			d="M8.5 7.7H10L13 11.7L10 15.7H8.5L11.5 11.7L8.5 7.7Z"
 			fill="#fff"
 		/>
 		<path
-			d="M13 7.7H14.5L17.5 11.7L14.5 15.7H13L16 11.7L13 7.7Z"
+			d="M11.5 7.7H13L16 11.7L13 15.7H11.5L14.5 11.7L11.5 7.7Z"
 			fill="#fff"
 		/>
 	</svg>
--- src/blocks/product-collection/icon.ts
+++ src/blocks/product-collection/icon.ts
@@ -8,21 +8,14 @@ const ClearanceIcon = createElement(
 		fill: 'none',
 	},
 	createElement( 'path', {
-		d: 'M3 5.5H6L10 11.5L6 17.5H3L7 11.5L3 5.5Z',
+		d: 'M6 5.5H9L13 11.5L9 17.5H6L10 11.5L6 5.5Z',
 		fill: 'none',
 		stroke: 'currentColor',
 		strokeWidth: 1.1,
 		strokeLinejoin: 'miter',
 	} ),
 	createElement( 'path', {
-		d: 'M9 5.5H12L16 11.5L12 17.5H9L13 11.5L9 5.5Z',
-		fill: 'none',
-		stroke: 'currentColor',
-		strokeWidth: 1.1,
-		strokeLinejoin: 'miter',
-	} ),
-	createElement( 'path', {
-		d: 'M15 5.5H18L22 11.5L18 17.5H15L19 11.5L15 5.5Z',
+		d: 'M12 5.5H15L19 11.5L15 17.5H12L16 11.5L12 5.5Z',
 		fill: 'none',
 		stroke: 'currentColor',
 		strokeWidth: 1.1,
