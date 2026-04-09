a6a45a1337a96dceb2dcd7972281811200c67155
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-09 12:28:36 +0800

    Update positioning

--- src/blocks/clearance-badge/icon.tsx
+++ src/blocks/clearance-badge/icon.tsx
@@ -1,10 +1,23 @@
 const ClearanceBadgeIcon = (
 	<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-		<rect x="1.5" y="6" width="21" height="12" rx="2" fill="currentColor" />
+		<rect
+			x="1.5"
+			y="5.7"
+			width="21"
+			height="12"
+			rx="2"
+			fill="currentColor"
+		/>
 
-		<path d="M7 8H8.5L11.5 12L8.5 16H7L10 12L7 8Z" fill="#fff" />
-		<path d="M10 8H11.5L14.5 12L11.5 16H10L13 12L10 8Z" fill="#fff" />
-		<path d="M13 8H14.5L17.5 12L14.5 16H13L16 12L13 8Z" fill="#fff" />
+		<path d="M7 7.7H8.5L11.5 11.7L8.5 15.7H7L10 11.7L7 7.7Z" fill="#fff" />
+		<path
+			d="M10 7.7H11.5L14.5 11.7L11.5 15.7H10L13 11.7L10 7.7Z"
+			fill="#fff"
+		/>
+		<path
+			d="M13 7.7H14.5L17.5 11.7L14.5 15.7H13L16 11.7L13 7.7Z"
+			fill="#fff"
+		/>
 	</svg>
 );
 
