d0c3e1a0e04c414b8d9ee3f8a4accd987da3bec7
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-17 10:25:35 +0800

    Fix lint issue

--- tests/e2e/0-bootstrap-payment-gateways.spec.js
+++ tests/e2e/0-bootstrap-payment-gateways.spec.js
@@ -1,6 +1,6 @@
 import { test as setup, expect } from '@wordpress/e2e-test-utils-playwright';
 
-setup( 'enable bank transfer payment gateway', async ( { requestUtils } ) => {
+setup( 'enable bacs payment gateway', async ( { requestUtils } ) => {
 	const gateway = await requestUtils.rest( {
 		method: 'PUT',
 		path: '/wc/v3/payment_gateways/bacs',
@@ -12,7 +12,7 @@ setup( 'enable bank transfer payment gateway', async ( { requestUtils } ) => {
 	expect( gateway.enabled ).toBe( true );
 } );
 
-setup( 'enable cash on delivery payment gateway', async ( { requestUtils } ) => {
+setup( 'enable cod payment gateway', async ( { requestUtils } ) => {
 	const gateway = await requestUtils.rest( {
 		method: 'PUT',
 		path: '/wc/v3/payment_gateways/cod',
