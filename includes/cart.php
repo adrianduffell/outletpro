104ec9a84679cd92409148ec322395fd6e3ca3ad
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-22 02:17:43 +0800

    Fix lint issues

--- includes/cart.php
+++ includes/cart.php
@@ -63,10 +63,10 @@ function add_clearance_to_cart_item_meta_hook( $item_data, $cart_item ): array {
 	array_unshift(
 		$item_data,
 		array(
-			'key'   => $clearance_label,
-			'value' => __( 'Yes', 'wc-clearance' ),
+			'key'     => $clearance_label,
+			'value'   => __( 'Yes', 'wc-clearance' ),
 			'display' => sprintf(
-			'<span class="wc-clearance-cart-item-meta">%s</span>',
+				'<span class="wc-clearance-cart-item-meta">%s</span>',
 				esc_html__( 'Yes', 'wc-clearance' )
 			),
 		)
