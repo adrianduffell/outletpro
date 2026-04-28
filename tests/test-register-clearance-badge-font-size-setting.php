f3da312d2742890dec268345e43b3764e7246524
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-28 21:29:05 +0800

    Remove non-valuable tests

--- tests/test-register-clearance-badge-font-size-setting.php
+++ tests/test-register-clearance-badge-font-size-setting.php
@@ -34,33 +34,6 @@ class Test_Register_Clearance_Badge_Font_Size_Setting extends WP_UnitTestCase {
 		$this->assertSame( 'string', $settings[ CLEARANCE_BADGE_FONT_SIZE_OPTION ]['type'] );
 	}
 
-	public function test_setting_default_is_empty_string(): void {
-		// Arrange.
-		unregister_setting( 'wc_clearance', CLEARANCE_BADGE_FONT_SIZE_OPTION );
-
-		// Act.
-		register_clearance_badge_font_size_setting();
-
-		// Assert.
-		$settings = get_registered_settings();
-		$this->assertSame( '', $settings[ CLEARANCE_BADGE_FONT_SIZE_OPTION ]['default'] );
-	}
-
-	public function test_setting_uses_css_value_sanitize_callback(): void {
-		// Arrange.
-		unregister_setting( 'wc_clearance', CLEARANCE_BADGE_FONT_SIZE_OPTION );
-
-		// Act.
-		register_clearance_badge_font_size_setting();
-
-		// Assert.
-		$settings = get_registered_settings();
-		$this->assertSame(
-			'WC_Clearance\\wc_clearance_sanitize_css_value',
-			$settings[ CLEARANCE_BADGE_FONT_SIZE_OPTION ]['sanitize_callback']
-		);
-	}
-
 	public function test_setting_is_shown_in_rest(): void {
 		// Arrange.
 		unregister_setting( 'wc_clearance', CLEARANCE_BADGE_FONT_SIZE_OPTION );
