ebd4df6a3576b584947b1ad3cc9b10388ff7ab7b
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-28 11:56:49 +0800

    Update naming

--- src/page-editor-sidebar/index.tsx
+++ src/page-editor-sidebar/index.tsx
@@ -2,19 +2,19 @@ import { PluginSidebar } from '@wordpress/edit-post';
 import { registerPlugin } from '@wordpress/plugins';
 import ClearanceIcon from './icon';
 
-const PLUGIN_NAME = 'wc-clearance-sidebar';
+const SIDEBAR_NAME = 'wc-clearance-sidebar';
 
-const ClearanceSidebar = () => {
+const ClearanceSectionSidebar = () => {
 	return (
 		<PluginSidebar
-			name={ PLUGIN_NAME }
-			title="Clearance section settings"
+			name={ SIDEBAR_NAME }
+			title="Clearance section"
 			isPinnable={ true }
 			icon={ ClearanceIcon }
 		/>
 	);
 };
 
-registerPlugin( PLUGIN_NAME, {
-	render: ClearanceSidebar,
+registerPlugin( SIDEBAR_NAME, {
+	render: ClearanceSectionSidebar,
 } );
