360980c7aef22eed368f657dddbfc94ba2c3bdb5
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-28 14:45:19 +0800

    Fix lint issues

--- includes/settings.php
+++ includes/settings.php
@@ -81,6 +81,12 @@ function wc_clearance_sanitize_css_value( string $value ): string {
  * @internal
  *
  * @param mixed $value The border value to sanitize.
+ * @return array{
+ *     color: string,
+ *     style: string,
+ *     width: string
+ * }
+ * @phpcsSuppress SlevomatCodingStandard.TypeHints
  */
 function wc_clearance_sanitize_border( $value ): array {
 	if ( ! is_array( $value ) ) {
