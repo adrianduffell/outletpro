9495a6c96d9a0d486d5f3366bca76afd983fbb51
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-04-28 11:57:41 +0800

    Add copy

--- src/page-editor-sidebar/index.tsx
+++ src/page-editor-sidebar/index.tsx
@@ -1,4 +1,6 @@
+import { PanelBody } from '@wordpress/components';
 import { PluginSidebar } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
 import { registerPlugin } from '@wordpress/plugins';
 import ClearanceIcon from './icon';
 
@@ -11,7 +13,16 @@ const ClearanceSectionSidebar = () => {
 			title="Clearance section"
 			isPinnable={ true }
 			icon={ ClearanceIcon }
-		/>
+		>
+			<PanelBody>
+				<p>
+					{ __(
+						'Customize the appearance of the clearance section. Changes apply to the whole site.'
+					) }
+				</p>
+				<h2>{ __( 'Badge' ) }</h2>
+			</PanelBody>
+		</PluginSidebar>
 	);
 };
 
