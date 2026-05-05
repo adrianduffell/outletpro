36f6ea8aa8bf3c636c8c6e512fda9183be3f3207
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-05-05 22:25:06 +0800

    Add canvas script

---
+++ assets/js/admin-canvas.js
@@ -0,0 +1,34 @@
+/**
+ * Admin canvas scripts.
+ */
+
+/**
+ * Fire a custom event when the editor canvas iframe is ready.
+ *
+ * This is used by the editor preview component to bind to the canvas iframe
+ * document and inject new styles.
+ *
+ * Todo: Consider contributing a patch upstream to provide a built-in way for block
+ * editor scripts to target the canvas iframe document.
+ */
+( () => {
+	document.addEventListener( 'DOMContentLoaded', () => {
+		const isEditorCanvas =
+			window.name === 'editor-canvas' ||
+			window.frameElement?.name === 'editor-canvas';
+
+		if ( isEditorCanvas && window.parent !== window ) {
+			window.parent.dispatchEvent(
+				new CustomEvent( 'wc-clearance-canvas-ready', {
+					detail: { document },
+				} )
+			);
+		}
+	} );
+} )();
+
+window.addEventListener('wc-clearance-canvas-ready', (e) => {
+
+	console.log('fired', e.detail);
+
+});
