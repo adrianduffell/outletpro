eeab1d7456189010ac37cec599dcee555d6f5969
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-05-13 02:28:20 +0800

    Add percentage to tooltip

--- src/page-editor-sidebar/index.tsx
+++ src/page-editor-sidebar/index.tsx
@@ -271,6 +271,7 @@ const ClearanceSectionSidebar = () => {
 							min={ 1 }
 							max={ 100 }
 							step={ 1 }
+							renderTooltipContent={ ( value ) => `${ value }%` }
 							allowReset={ false }
 							withInputField={ false }
 							__next40pxDefaultSize
