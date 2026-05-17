7779a237041f88ee6b2ec5fd6cedd5bc86020e6a
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-05-17 22:08:20 +0800

    Revert to integer type

--- includes/settings.php
+++ includes/settings.php
@@ -474,7 +474,7 @@ function register_clearance_badge_scale_setting(): void {
 			'sanitize_callback' => 'WC_Clearance\sanitize_unsigned_integer',
 			'show_in_rest'      => array(
 				'schema' => array(
-					'type'    => array( 'integer', 'null' ),
+					'type'    => 'integer',
 					'minimum' => 0,
 				),
 			),
