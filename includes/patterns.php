afdb070020616d88c13a0d99118536bde2e1e745
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-05-27 13:31:05 +0800

    Revert to outline style

--- includes/patterns.php
+++ includes/patterns.php
@@ -205,8 +205,8 @@ function get_outlet_filter_tiles_content(): string {
 		$label     = wp_kses_post( sprintf( $label_template, wp_strip_all_tags( wc_price( $price, array( 'decimals' => 0 ) ) ) ) );
 		$href      = esc_url( add_query_arg( 'max_price', $price, $base_url ) );
 		$buttons[] =
-			'<!-- wp:button {"className":"is-style-fill","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}},"typography":{"lineHeight":"1.6"}}} -->' . "\n" .
-			'<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" href="' . $href . '" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;line-height:1.6">' . $label . '</a></div>' . "\n" .
+			'<!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}},"typography":{"lineHeight":"1.6"}}} -->' . "\n" .
+			'<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" href="' . $href . '" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;line-height:1.6">' . $label . '</a></div>' . "\n" .
 			'<!-- /wp:button -->';
 	}
 
