7d210dbbdc0f39479be2c7f3da6d271077ca0236
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-05-31 00:19:49 +0800

    Fix unregister errors

--- tests/test-register-outlet-sort-filter-pattern.php
+++ tests/test-register-outlet-sort-filter-pattern.php
@@ -13,7 +13,7 @@ class Test_Register_Outlet_Sort_Filter_Pattern extends WP_UnitTestCase {
 
 	public function test_pattern_is_registered_after_register_outlet_sort_filter_pattern(): void {
 		// Arrange.
-		unregister_block_pattern( 'wc-outlet/outlet-sort-filter' );
+		$this->deinit_patterns();
 
 		// Act.
 		register_outlet_sort_filter_pattern();
@@ -24,7 +24,7 @@ class Test_Register_Outlet_Sort_Filter_Pattern extends WP_UnitTestCase {
 
 	public function test_pattern_has_expected_title_and_description(): void {
 		// Arrange.
-		unregister_block_pattern( 'wc-outlet/outlet-sort-filter' );
+		$this->deinit_patterns();
 
 		// Act.
 		register_outlet_sort_filter_pattern();
@@ -36,6 +36,9 @@ class Test_Register_Outlet_Sort_Filter_Pattern extends WP_UnitTestCase {
 	}
 
 	public function test_get_outlet_sort_filter_pattern_content_contains_html_block_markup(): void {
+		// Arrange.
+		$this->deinit_patterns();
+
 		// Act.
 		$content = get_outlet_sort_filter_pattern_content();
 
@@ -45,5 +48,16 @@ class Test_Register_Outlet_Sort_Filter_Pattern extends WP_UnitTestCase {
 		$this->assertStringContainsString( 'data-wp-block-html="js"', $content );
 	}
 
-
+	/**
+	 * Temporary function until one is added to patterns.php.
+	 */
+	private function deinit_patterns() {
+		$registry = \WP_Block_Patterns_Registry::get_instance();
+		if ( $registry->is_registered( 'wc-outlet/outlet-sort-filter' ) ) {
+			$registry->unregister( 'wc-outlet/outlet-sort-filter' );
+		}
+		if ( $registry->is_registered( 'wc-outlet/outlet-filter-tiles' ) ) {
+			$registry->unregister( 'wc-outlet/outlet-filter-tiles' );
+		}
+	}
 }
