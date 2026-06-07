2235b598c6beec9d9954be7832baaa4c3f3493b6
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-06-08 00:36:58 +0800

    Add HPOS compatibility declaration (#814)
    * Add HPOS compatability declaration
    
    * Fix lint issue
    
    * Add class_exists check

--- outletpro.php
+++ outletpro.php
@@ -141,3 +141,13 @@ function activate(): void {
 }
 
 register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
+
+add_action(
+	'before_woocommerce_init',
+	function (): void {
+		if ( ! class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			return;
+		}
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', PLUGIN_FILE, true );
+	}
+);
