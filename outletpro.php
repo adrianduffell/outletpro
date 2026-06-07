3c4003591c9041a0c9ec1a146c7a149595e85b89
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-06-08 00:09:18 +0800

    Fix lint issue

--- outletpro.php
+++ outletpro.php
@@ -139,8 +139,7 @@ function activate(): void {
 
 register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
 
-add_action( 'before_woocommerce_init', function() {
-	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
-		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
-	}
-} );
+add_action(
+	'before_woocommerce_init',
+	fn() => \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true )
+);
