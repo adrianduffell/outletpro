7707803b72c4b6b72295266be780b0ff046f8abc
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-06-07 23:47:42 +0800

    Add HPOS compatability declaration

--- outletpro.php
+++ outletpro.php
@@ -138,3 +138,9 @@ function activate(): void {
 }
 
 register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
+
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
