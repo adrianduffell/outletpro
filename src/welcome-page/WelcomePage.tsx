2e6a8b06db56eda0af3b2dc640129c52430c1059
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-07-12 00:16:45 +0800

    Update layout

--- src/welcome-page/WelcomePage.tsx
+++ src/welcome-page/WelcomePage.tsx
@@ -66,61 +66,75 @@ export function WelcomePage(): JSX.Element {
 
 	if ( isSuccess ) {
 		return (
-			<div className="outletpro-welcome-page">
-				<p>
-					{ __(
-						'🎉 Success! Outlet Pro is now set up.',
-						'outletpro'
-					) }
-				</p>
-				<p>
-					{ __(
-						"Get started by including your first product in the store's outlet.",
-						'outletpro'
-					) }{ ' ' }
-					<a href={ outletproWelcomePage.productsUrl }>
-						{ __( 'Products', 'outletpro' ) }
-					</a>{ ' ' }
-					<a href="https://outletpro.zip/">
-						{ __( 'Learn more', 'outletpro' ) }
-					</a>
-				</p>
+			<div className="outletpro-welcome-page-wrapper">
+				<div className="outletpro-welcome-page">
+					<h1 style={ {  fontSize: '2.8rem', fontWeight: 600, textAlign: 'center' } }>🎉 Success!</h1>
+					<p style={ { fontSize: '1.1rem', fontWeight:300, margin: '1rem auto', maxWidth:'90%', textAlign: 'center' } }>
+						{ __(
+							"Outlet Pro is now set up. Get started by including your first product in the store's outlet.",
+							'outletpro'
+						) }{ ' ' }
+						<a href="https://outletpro.zip/help/get-started/" target="_blank" rel="noopener">
+							{ __( 'Learn More', 'outletpro' ) }
+						</a>
+					</p>
+					<div style={ { marginTop: '2rem', textAlign: 'center' } }>
+						<Button
+							variant="primary"
+							href={ outletproWelcomePage.productsUrl }
+						>
+							{ __( 'Get Started', 'outletpro' ) }
+						</Button>
+					</div>
+				</div>
 			</div>
 		);
 	}
 
 	return (
+		<div className="outletpro-welcome-page-wrapper">
 		<div className="outletpro-welcome-page">
-			<p>
+			<h1 style={ {  fontSize: '2.8rem', fontWeight: 600, textAlign: 'center' } }>Welcome to Outlet Pro!</h1>
+			<p style={ { fontSize: '1.1rem', fontWeight:300, margin: '1rem auto', maxWidth:'80%', textAlign: 'center' } }>
 				{ __(
-					'Thank you for installing Outlet Pro. Enter your premium license key to complete setup.',
+					'Thank you for installing Outlet Pro. Please enter your premium license key to begin setup.',
 					'outletpro'
-				) }{ ' ' }
-				<a href="https://outletpro.zip/">
-					{ __( 'Learn more', 'outletpro' ) }
-				</a>
+				) }
 			</p>
-			<TextControl
-				label={ __( 'Premium license key', 'outletpro' ) }
-				value={ licenseKey }
-				onChange={ setLicenseKey }
-				placeholder="XXXX-XXXX-XXXX"
-				// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
-				__next40pxDefaultSize
-			/>
+			<div style={ { margin: '2rem auto', maxWidth:'90%' } }>
+				<TextControl
+					label={ __( 'Premium license key', 'outletpro' ) }
+					hideLabelFromVision={ true }
+					value={ licenseKey }
+					help={ __( 'You can find your license key in the email sent after purchasing Outlet Pro.', 'outletpro' ) }
+					onChange={ setLicenseKey }
+					placeholder="XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX"
+					// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+					__next40pxDefaultSize
+				/>
+			</div>
 			{ errorMessage && (
 				<p className="outletpro-welcome-page__error">
 					{ errorMessage }
 				</p>
 			) }
-			<Button
-				variant="primary"
-				onClick={ handleContinue }
-				isBusy={ isLoading }
-				disabled={ isLoading }
-			>
-				{ __( 'Continue', 'outletpro' ) }
-			</Button>
+			<div style={ { marginTop: '2rem', textAlign: 'center' } }>
+
+				<p style={ { margin: '2rem', textAlign: 'center' } }><a href="https://outletpro.zip/help/license-key/" target="_blank" rel="noopener">
+					{ __( 'I don’t have a license key', 'outletpro' ) }
+				</a>
+				</p>
+				<Button
+					variant="primary"
+					onClick={ handleContinue }
+					isBusy={ isLoading }
+					disabled={ isLoading }
+					style={ { marginLeft: '1em' } }
+				>
+					{ __( 'Continue', 'outletpro' ) }
+				</Button>
+			</div>
 		</div>
+	</div>
 	);
 }
