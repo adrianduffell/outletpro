3d9fe3e9807fc3db0a3a804200401b6eab4ad4bb
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-07-13 11:14:47 +0800

    Remove duplicate h1

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -95,7 +95,6 @@ function render_welcome_page(): void {
 	}
 	?>
 	<div class="wrap">
-		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 		<div id="outletpro-welcome-page"></div>
 	</div>
 	<?php
