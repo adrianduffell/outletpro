2feb97dff579904de1b5e9260d6cc7d88249bfa5
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-07-25 18:10:18 +0800

    Update test

--- tests/test-invalidate-license-cache-hook.php
+++ tests/test-invalidate-license-cache-hook.php
@@ -5,17 +5,15 @@
  * @package OutletPro
  */
 
-use function OutletPro\deinit_license;
 use function OutletPro\has_license;
-use function OutletPro\init_license;
+use function OutletPro\init_settings;
 use const OutletPro\LICENSE_KEY_OPTION;
 
 class Test_Invalidate_License_Cache_Hook extends WP_UnitTestCase {
 
 	public function test_invalidates_transient_when_license_key_is_added(): void {
 		// Arrange.
-		deinit_license();
-		init_license();
+		init_settings();
 		delete_option( LICENSE_KEY_OPTION );
 		$this->assertFalse( has_license() );
 
@@ -24,15 +22,11 @@ class Test_Invalidate_License_Cache_Hook extends WP_UnitTestCase {
 
 		// Assert.
 		$this->assertTrue( has_license() );
-
-		// Cleanup.
-		deinit_license();
 	}
 
 	public function test_invalidates_transient_when_license_key_is_updated(): void {
 		// Arrange.
-		deinit_license();
-		init_license();
+		init_settings();
 		update_option( LICENSE_KEY_OPTION, '0' );
 		$this->assertFalse( has_license() );
 
@@ -41,8 +35,5 @@ class Test_Invalidate_License_Cache_Hook extends WP_UnitTestCase {
 
 		// Assert.
 		$this->assertTrue( has_license() );
-
-		// Cleanup.
-		deinit_license();
 	}
 }
