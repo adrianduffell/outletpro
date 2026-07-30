1256d2584366c0d5d969ce723a7eda42a5dc85e9
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-07-30 22:33:56 +0800

    Add plugin file tests

---
+++ tests/test-plugin-file.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Tests the plugin file.
+ *
+ * @package OutletPro
+ */
+
+use const OutletPro\PLUGIN_FILE;
+
+class Test_Plugin_File extends WP_UnitTestCase {
+
+	/**
+	 * Test the Update URI plugin header.
+	 *
+	 * @group updates
+	 */
+	public function test_update_uri(): void {
+		// Arrange.
+		$plugin_data = get_plugin_data( PLUGIN_FILE );
+
+		// Action.
+
+		// Assert.
+		$this->assertSame( 'https://adrianduffell.store/outletpro', $plugin_data['UpdateURI'] );
+	}
+
+	public function test_requires_plugins(): void {
+		// Arrange.
+		$plugin_data = get_plugin_data( PLUGIN_FILE );
+
+		// Action.
+
+		// Assert.
+		$this->assertSame( 'woocommerce', $plugin_data['RequiresPlugins'] );
+	}
+}
