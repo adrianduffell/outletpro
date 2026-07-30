9fe7a5a60701387e443d94806ba7750f21ee9f3d
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-07-30 16:13:30 +0800

    Remove debug code

--- bin/preprocess-marketplace.sh
+++ bin/preprocess-marketplace.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 for file in outletpro.php src/index.ts; do
-	echo "Processing: $file"
-
 	sed -E \
 		's@^([[:space:]]*)//[[:space:]]*(#(ifdef|endif).*)$@\1\2@' \
 		"$file" |
@@ -11,10 +9,7 @@ for file in outletpro.php src/index.ts; do
 	mv "$file.tmp" "$file"
 done
 
-echo "Removing license/update files..."
-
 grep -rEl --include='*.php' '@subpackage (License|Updates)' includes |
 while IFS= read -r file; do
-	echo "Deleting: $file"
 	rm "$file"
 done
