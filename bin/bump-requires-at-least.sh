c408d39ef751b12b00490aa606cfea15b6ea8b0e
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-07-31 22:16:17 +0800

    Fix “Requires at least” not updated in readme.txt

--- bin/bump-requires-at-least.sh
+++ bin/bump-requires-at-least.sh
@@ -7,8 +7,14 @@ if [ -z "$version" ]; then
 	exit 1
 fi
 
+# Plugin header.
 sed -i.bak -E \
 	"s/^([[:space:]]*\*[[:space:]]*Requires at least:).*/\1 $version/" \
 	outletpro.php
-
 rm outletpro.php.bak
+
+# readme.txt.
+sed -i.bak -E \
+	"s/^(Requires at least:).*/\1 $version/" \
+	readme.txt
+rm readme.txt.bak
