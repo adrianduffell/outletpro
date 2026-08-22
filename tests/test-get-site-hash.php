a36c74908b934c50707e016b1256c3a14e7f558a
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-08-22 22:57:47 +0800

    Remove bad test

--- tests/test-get-site-hash.php
+++ tests/test-get-site-hash.php
@@ -27,28 +27,4 @@ class Test_Get_Site_Hash extends WP_UnitTestCase {
 		// Assert.
 		$this->assertSame( '73d986e00906', $site_hash );
 	}
-
-	public function test_requests_root_home_url_over_http(): void { // phpcs:ignore Generic.Metrics.NestingLevel.MaxExceeded
-		// Arrange.
-		$requested_path   = null;
-		$requested_scheme = null;
-		add_filter(
-			'home_url',
-			function ( $url, $path, $scheme ) use ( &$requested_path, &$requested_scheme ): string {
-				$requested_path   = $path;
-				$requested_scheme = $scheme;
-
-				return 'http://example.com/';
-			},
-			10,
-			3
-		);
-
-		// Act.
-		get_site_hash();
-
-		// Assert.
-		$this->assertSame( '/', $requested_path );
-		$this->assertSame( 'http', $requested_scheme );
-	}
 }
