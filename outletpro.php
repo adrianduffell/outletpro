ba8b79b97c62b998e685127c5f5b1b05a539b7ef
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-08-24 19:19:34 +0800

    Rename files (#1046)

--- outletpro.php
+++ outletpro.php
@@ -44,7 +44,7 @@ require_once __DIR__ . '/includes/system-status.php';
 require_once __DIR__ . '/includes/taxonomies.php';
 require_once __DIR__ . '/includes/rest-api.php';
 // #ifdef LICENSE
-require_once __DIR__ . '/includes/admin-menu.php';
+require_once __DIR__ . '/includes/admin-menu-license.php';
 // #endif
 require_once __DIR__ . '/includes/admin-product-options.php';
 require_once __DIR__ . '/includes/admin-product-bulk-edit.php';
@@ -53,7 +53,7 @@ require_once __DIR__ . '/includes/shortcodes.php';
 require_once __DIR__ . '/includes/settings.php';
 // #ifdef LICENSE
 require_once __DIR__ . '/includes/settings-license.php';
-require_once __DIR__ . '/includes/license.php';
+require_once __DIR__ . '/includes/admin-plugins-license.php';
 require_once __DIR__ . '/includes/enqueue-license.php';
 // #endif
 require_once __DIR__ . '/includes/patterns.php';
