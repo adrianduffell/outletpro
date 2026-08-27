df139cdda5cc2389186488fd678514e26862e024
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-08-27 17:05:02 +0800

    Add mock_http_rest_api_response() (#1065)

---
+++ tests/mock-http-rest-api-response.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * REST API response test helper.
+ *
+ * @package OutletPro
+ * @group license
+ * @copyright 2026 Adrian Duffell
+ * @license GNU General Public License v2.0 or later
+ */
+
+/**
+ * Mock a JSON REST API response.
+ *
+ * @param string $request_method HTTP request method.
+ * @param string $endpoint License API endpoint URL.
+ * @param string $json Response JSON.
+ * @param int    $response_code HTTP response code.
+ */
+function mock_http_rest_api_response( string $request_method, string $endpoint, string $json, int $response_code = 200 ): void { // phpcs:ignore Generic.Metrics.NestingLevel.MaxExceeded
+	add_filter(
+		'pre_http_request',
+		function ( $pre, $args, $url ) use ( $request_method, $endpoint, $json, $response_code ) {
+			if ( $endpoint !== $url ) {
+				return $pre;
+			}
+
+			if ( ( $args['method'] ?? null ) !== $request_method ) {
+				return $pre;
+			}
+
+			return array(
+				'headers'  => array( 'Content-Type' => 'application/json' ),
+				'body'     => $json,
+				'response' => array(
+					'code'    => $response_code,
+					'message' => get_status_header_desc( $response_code ),
+				),
+				'cookies'  => array(),
+				'filename' => null,
+			);
+		},
+		10,
+		3
+	);
+}
