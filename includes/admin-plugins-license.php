3b37b4aef6d08f5e14b671771474b9d01393d0c3
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2026-08-30 19:49:30 +0800

    Fix stray whitespace around icon in plugin notice
    Fixes extra whitespace around icon in plugin notice, caused by HTML source formatting

--- includes/admin-plugins-license.php
+++ includes/admin-plugins-license.php
@@ -85,8 +85,7 @@ function add_premium_license_notice_hook( string $plugin_file, array $plugin_dat
 	<tr class="plugin-update-tr active outletpro-license-notice">
 		<td colspan="4" class="plugin-update colspanchange">
 			<div class="update-message notice inline notice-warning notice-alt">
-				<p>
-					<?php esc_html_e( 'A premium license is needed for Outlet Pro to receive updates.', 'outletpro' ); ?>
+				<p><?php esc_html_e( 'A premium license is needed for Outlet Pro to receive updates.', 'outletpro' ); ?>
 					<a class="button-link" href="<?php echo esc_url( admin_url( 'admin.php?page=outletpro-welcome' ) ); ?>">
 						<?php esc_html_e( 'Set up premium license', 'outletpro' ); ?>
 					</a>
